ba41bca3f1a8e77059d255347fe38abe17374e28
gabrielmoliva <moliva.gabriel@gmail.com>
2025-07-01 18:06:49 -0300

    add config.php para configuração do banco de dados

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+    define("HOST","localhost");
+    define("USER","root");
+    define("PASS","root");
+    define("BASE","visu_clientes");
+
+    $conn = new mysqli(HOST, USER, PASS, BASE);
