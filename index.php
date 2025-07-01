b336b9584f3c76193acd4b079a38caa036033e0e
gabrielmoliva <moliva.gabriel@gmail.com>
2025-07-01 18:57:37 -0300

    criação da navbar

--- index.php
+++ index.php
@@ -1,16 +1,61 @@
+<!doctype html>
+<html lang="pt-BR">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>CRUD Clientes</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+</head>
+
+<body>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">CRUD Clientes</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Listar</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Cadastrar</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Editar</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Excluir</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q"
+    crossorigin="anonymous"></script>
+</body>
+
+</html>
+
 <?php
 
-$host = 'localhost:3306'; 
-$dbuser = 'root';
-$dbpassword = 'root';
-$dbname = 'visu_clientes';
+  include("config.php");
+  /*
+  $host = 'localhost:3306'; 
+  $dbuser = 'root';
+  $dbpassword = 'root';
+  $dbname = 'visu_clientes';
 
-$conn = mysqli_connect($host,$dbuser,$dbpassword,$dbname);
-if($conn){
-    mysqli_query($conn,'SELECT COUNT(1) AS total FROM `clientes`;');
+  $conn = new mysqli($host,$dbuser,$dbpassword,$dbname);
+  */
+  if ($conn) {
+    //mysqli_query($conn,'SELECT COUNT(1) AS total FROM `clientes`;');
     echo "Connection - successful </br>";
-}
-else {
-    echo "Connection - failed </br>" . mysqli_connect_error();
-}
-
+  } else {
+    echo "Connection - failed </br>"; //. mysqli_connect_error();
+  }
