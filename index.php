ee83984adcfd3cca4c2988747e5bccce07798000
gabrielmoliva <moliva.gabriel@gmail.com>
2025-07-02 11:37:08 -0300

    criando a tabela clientes automaticamente e associando os itens da navbar às devidas páginas

--- index.php
+++ index.php
@@ -20,21 +20,55 @@
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Listar</a>
+            <a class="nav-link active" aria-current="page" href="?page=listar">Listar</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Cadastrar</a>
+            <a class="nav-link active" aria-current="page" href="?page=cadastrar">Cadastrar</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Editar</a>
+            <a class="nav-link active" aria-current="page" href="?page=editar">Editar</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Excluir</a>
+            <a class="nav-link active" aria-current="page" href="?page=excluir">Excluir</a>
           </li>
         </ul>
       </div>
     </div>
   </nav>
+
+  <div class="container">
+    <div class="row">
+      <div class="col mt-5">
+        <?php
+        include("config.php");
+        include("cliente_model.php");
+
+        switch ($_REQUEST["page"]) {
+          case "listar":
+            include("listar_clientes.php");
+            break;
+          case "cadastrar":
+            include("cadastro_cliente.php");
+            break;
+          case "editar":
+            include("editar_cliente.php");
+            break;
+          case "excluir":
+            include("excluir_cliente");
+            break;
+          case "service":
+            include("cliente_service.php");
+            break;
+          default:
+            // placeholder
+            echo "hello";
+            break;
+        }
+        ?>
+      </div>
+    </div>
+  </div>
+
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q"
     crossorigin="anonymous"></script>
@@ -43,19 +77,20 @@
 </html>
 
 <?php
-
   include("config.php");
-  /*
-  $host = 'localhost:3306'; 
-  $dbuser = 'root';
-  $dbpassword = 'root';
-  $dbname = 'visu_clientes';
 
-  $conn = new mysqli($host,$dbuser,$dbpassword,$dbname);
-  */
-  if ($conn) {
-    //mysqli_query($conn,'SELECT COUNT(1) AS total FROM `clientes`;');
-    echo "Connection - successful </br>";
-  } else {
-    echo "Connection - failed </br>"; //. mysqli_connect_error();
+  // Optei por tornar o telefone a chave primária da tabela para simplificar o case, mas,
+  // em caso de um projeto maior, optaria por um campo de id auto incremental 
+  // e deixaria o telefone como NOT NULL UNIQUE
+  $sql = "CREATE TABLE IF NOT EXISTS clientes (
+    nome VARCHAR(100) NOT NULL,
+    telefone VARCHAR(20) PRIMARY KEY,
+    data_nascimento DATE NOT NULL,
+    cpf VARCHAR(14) DEFAULT NULL,
+    email VARCHAR(100) DEFAULT NULL
+  )";
+
+  if ($conn->query($sql)) {
+    echo "foi";
   }
+?>
